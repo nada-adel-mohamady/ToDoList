cc23d6990a31a77f168a4da3d6e12c8aa2f77180
sara <sara.yasser.kamal@gmail.com>
2020-03-31 11:08:24 +0200

    dialog box without styling

--- ToDoList2/app/src/main/java/com/example/todolist/MainActivity.java
+++ ToDoList2/app/src/main/java/com/example/todolist/MainActivity.java
@@ -5,22 +5,38 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import static com.example.todolist.R.*;
+import static com.example.todolist.R.id.button2;
+import static com.example.todolist.R.layout.dialog_custom;
 
 
 public class MainActivity extends AppCompatActivity {
+
+    Dialog epicDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        setContentView(R.layout.activity_main);
-        Toolbar toolbar =  findViewById(R.id.toolbar);
+        epicDialog = new Dialog(this);
+
+        setContentView(layout.activity_main);
+        Toolbar toolbar =  findViewById(id.toolbar);
         setSupportActionBar(toolbar);
+
     }
 
 
@@ -37,12 +53,23 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         switch (item.getItemId()){
-            case R.id.reminder:
+            case id.reminder:
+
+
+                AlertDialog.Builder mBilder = new AlertDialog.Builder(MainActivity.this);
+                View mView = getLayoutInflater().inflate(layout.custom_dialog, null);
                 Toast.makeText(this, "reminder selected", Toast.LENGTH_SHORT).show();
-                dialog_custom dialogCustom = new dialog_custom();
-                dialogCustom.show(getSupportFragmentManager(), "exaaaaaample");
+
+                Button cancleBtn = (Button) mView.findViewById(id.cancleBtn);
+                Button commitBtn = (Button) mView.findViewById(id.commitBtn);
+                CheckBox important = (CheckBox) mView.findViewById(id.important);
+                EditText multiLineText = (EditText) mView.findViewById(id.editText2);
+
+                mBilder.setView(mView);
+                AlertDialog dialogCreater = mBilder.create();
+                dialogCreater.show();
                 return true;
-            case R.id.exit:
+            case id.exit:
             Toast.makeText(this, "exit selected", Toast.LENGTH_SHORT).show();
             return true;
             default:
